22f9c23987aeaf3f825600278f2b047e7a4af0b6
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2024-03-05 14:44:44 +0700

    route

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::any('/halo', function () {
+//     return 'halo dunia';
+// });
+
+Route::match(['post', 'get'],'/halo', function () {
+    return 'halo dunia';
+});
+
+Route::get('/halo/{nama}', function ($nama) {
+    return 'halo ' . $nama;
+});
