2ab0d06d6214bf98b8557aa75af92ccc0c5c0635
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2024-03-26 13:41:13 +0700

    laravel 11

--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,7 @@
 <?php
 
-use App\Http\Controllers\BukuController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
-
-// Route::any('/halo', function () {
-//     return 'halo dunia';
-// });
-
-Route::match(['post', 'get'],'/halo', function () {
-    return 'halo dunia';
-});
-
-Route::get('/halo/{nama}', function ($nama) {
-    return 'halo ' . $nama;
-});
-
-Route::get('/buku', [BukuController::class, 'daftar']);
-
-Route::post('/buku', [BukuController::class, 'tambah']);
